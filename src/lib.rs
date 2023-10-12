1f7224432d38f2c06fe9472610ea2fe4d92f061d
martinohmann <martinohmann@gmail.com>
2023-10-12 22:56:13 +0200

    refactor!: remove `try_` methods

--- src/lib.rs
+++ src/lib.rs
@@ -41,172 +41,144 @@ impl fmt::Display for AccessError {
 #[cfg(feature = "std")]
 impl std::error::Error for AccessError {}
 
-macro_rules! impl_primitive_access {
-    ($ty:ty, $get:ident, $try_get:ident) => {
-        #[inline]
-        fn $get(&self, field: &str) -> Option<$ty> {
-            self.$try_get(field).ok()
-        }
-
-        #[inline]
-        fn $try_get(&self, field: &str) -> Result<$ty, AccessError> {
-            self.try_get_any(field)
-                .and_then(try_downcast_ref::<$ty>)
-                .map(|v| *v)
-        }
+macro_rules! primitive_methods {
+    ($($ty:ty => $get:ident),*) => {
+        $(
+            #[inline]
+            fn $get(&self, field: &str) -> Result<$ty, AccessError> {
+                self.get_any(field)
+                    .and_then(try_downcast_ref)
+                    .map(|v| *v)
+            }
+        )*
     };
 }
 
+macro_rules! first_ok {
+    ($value:expr, $($ty:ty => $map:expr),*) => {{
+        $(if let Some(value) = $value.downcast_ref::<$ty>().map($map) {
+            return Ok(value);
+        })*
+
+        return Err(AccessError::TypeMismatch);
+    }};
+}
+
 pub trait FieldAccess {
-    #[inline]
-    fn get_any(&self, field: &str) -> Option<&dyn Any> {
-        self.try_get_any(field).ok()
-    }
+    fn get_any(&self, field: &str) -> Result<&dyn Any, AccessError>;
 
-    fn try_get_any(&self, field: &str) -> Result<&dyn Any, AccessError>;
-
-    #[inline]
-    fn get_any_mut(&mut self, field: &str) -> Option<&mut dyn Any> {
-        self.try_get_any_mut(field).ok()
-    }
-
-    fn try_get_any_mut(&mut self, field: &str) -> Result<&mut dyn Any, AccessError>;
-
-    impl_primitive_access!(bool, get_bool, try_get_bool);
-    impl_primitive_access!(u8, get_u8, try_get_u8);
-    impl_primitive_access!(u16, get_u16, try_get_u16);
-    impl_primitive_access!(u32, get_u32, try_get_u32);
-    impl_primitive_access!(u64, get_u64, try_get_u64);
-    impl_primitive_access!(u128, get_u128, try_get_u128);
-    impl_primitive_access!(i8, get_i8, try_get_i8);
-    impl_primitive_access!(i16, get_i16, try_get_i16);
-    impl_primitive_access!(i32, get_i32, try_get_i32);
-    impl_primitive_access!(i64, get_i64, try_get_i64);
-    impl_primitive_access!(i128, get_i128, try_get_i128);
-    impl_primitive_access!(f32, get_f32, try_get_f32);
-    impl_primitive_access!(f64, get_f64, try_get_f64);
-
-    #[inline]
-    fn get_str(&self, field: &str) -> Option<&str> {
-        self.try_get_str(field).ok()
-    }
+    fn get_any_mut(&mut self, field: &str) -> Result<&mut dyn Any, AccessError>;
 
     #[cfg(feature = "alloc")]
     #[inline]
-    fn try_get_str(&self, field: &str) -> Result<&str, AccessError> {
-        self.try_get_any(field)
-            .and_then(|value| match value.downcast_ref::<&str>() {
-                Some(v) => Ok(*v),
-                None => try_downcast_ref::<String>(value).map(String::as_str),
-            })
+    fn get_str(&self, field: &str) -> Result<&str, AccessError> {
+        self.get_any(field)
+            .and_then(|value| first_ok!(value, &str => |v| *v, String => String::as_str))
     }
 
     #[cfg(not(feature = "alloc"))]
     #[inline]
-    fn try_get_str(&self, field: &str) -> Result<&str, AccessError> {
-        self.try_get_any(field)
-            .and_then(try_downcast_ref::<&str>)
-            .map(|v| *v)
+    fn get_str(&self, field: &str) -> Result<&str, AccessError> {
+        self.get_any(field).and_then(try_downcast_ref).map(|v| *v)
+    }
+
+    primitive_methods! {
+        bool => get_bool,
+        u8 => get_u8,
+        u16 => get_u16,
+        u32 => get_u32,
+        u64 => get_u64,
+        u128 => get_u128,
+        i8 => get_i8,
+        i16 => get_i16,
+        i32 => get_i32,
+        i64 => get_i64,
+        i128 => get_i128,
+        f32 => get_f32,
+        f64 => get_f64
     }
 }
 
-impl dyn FieldAccess + '_ {
+impl dyn FieldAccess {
     #[inline]
-    pub fn get<T: Any>(&self, field: &str) -> Option<&T> {
-        self.try_get(field).ok()
+    pub fn get<T: Any>(&self, field: &str) -> Result<&T, AccessError> {
+        self.get_any(field).and_then(try_downcast_ref)
     }
 
     #[inline]
-    pub fn try_get<T: Any>(&self, field: &str) -> Result<&T, AccessError> {
-        self.try_get_any(field).and_then(try_downcast_ref::<T>)
-    }
-
-    #[inline]
-    pub fn get_mut<T: Any>(&mut self, field: &str) -> Option<&mut T> {
-        self.try_get_mut(field).ok()
-    }
-
-    #[inline]
-    pub fn try_get_mut<T: Any>(&mut self, field: &str) -> Result<&mut T, AccessError> {
-        self.try_get_any_mut(field).and_then(try_downcast_mut::<T>)
-    }
-
-    #[inline]
-    pub fn get_slice<T: Any>(&self, field: &str) -> Option<&[T]> {
-        self.try_get_slice(field).ok()
+    pub fn get_mut<T: Any>(&mut self, field: &str) -> Result<&mut T, AccessError> {
+        self.get_any_mut(field).and_then(try_downcast_mut)
     }
 
     #[cfg(feature = "alloc")]
     #[inline]
-    pub fn try_get_slice<T: Any>(&self, field: &str) -> Result<&[T], AccessError> {
-        self.try_get_any(field)
-            .and_then(|value| match value.downcast_ref::<&[T]>() {
-                Some(v) => Ok(*v),
-                None => try_downcast_ref::<Vec<T>>(value).map(Vec::as_slice),
-            })
+    pub fn get_slice<T: Any>(&self, field: &str) -> Result<&[T], AccessError> {
+        self.get_any(field)
+            .and_then(|value| first_ok!(value, &[T] => |v| *v, Vec<T> => Vec::as_slice))
     }
 
     #[cfg(not(feature = "alloc"))]
     #[inline]
-    pub fn try_get_slice<T: Any>(&self, field: &str) -> Result<&[T], AccessError> {
-        self.try_get_any(field)
-            .and_then(try_downcast_ref::<&[T]>)
-            .map(|v| *v)
+    pub fn get_slice<T: Any>(&self, field: &str) -> Result<&[T], AccessError> {
+        self.get(field).map(|v| *v)
     }
 
     #[inline]
     pub fn set<T: Any>(&mut self, field: &str, value: T) -> Result<(), AccessError> {
-        let f = self.try_get_mut(field)?;
-        *f = value;
-        Ok(())
+        self.replace(field, value).map(|_| ())
+    }
+
+    #[inline]
+    pub fn replace<T: Any>(&mut self, field: &str, value: T) -> Result<T, AccessError> {
+        Ok(core::mem::replace(self.get_mut(field)?, value))
+    }
+
+    #[inline]
+    pub fn take<T: Any + Default>(&mut self, field: &str) -> Result<T, AccessError> {
+        Ok(core::mem::take(self.get_mut(field)?))
     }
 }
 
-macro_rules! forward_field_access_methods {
+macro_rules! forward_dyn_methods {
     () => {
         #[inline]
-        pub fn get<T: Any>(&self, field: &str) -> Option<&T> {
+        pub fn get<T: Any>(&self, field: &str) -> Result<&T, AccessError> {
             <dyn FieldAccess>::get(self, field)
         }
 
         #[inline]
-        pub fn try_get<T: Any>(&self, field: &str) -> Result<&T, AccessError> {
-            <dyn FieldAccess>::try_get(self, field)
-        }
-
-        #[inline]
-        pub fn get_mut<T: Any>(&mut self, field: &str) -> Option<&mut T> {
+        pub fn get_mut<T: Any>(&mut self, field: &str) -> Result<&mut T, AccessError> {
             <dyn FieldAccess>::get_mut(self, field)
         }
 
         #[inline]
-        pub fn try_get_mut<T: Any>(&mut self, field: &str) -> Result<&mut T, AccessError> {
-            <dyn FieldAccess>::try_get_mut(self, field)
-        }
-
-        #[inline]
-        pub fn get_slice<T: Any>(&self, field: &str) -> Option<&[T]> {
+        pub fn get_slice<T: Any>(&self, field: &str) -> Result<&[T], AccessError> {
             <dyn FieldAccess>::get_slice(self, field)
-        }
-
-        #[inline]
-        pub fn try_get_slice<T: Any>(&self, field: &str) -> Result<&[T], AccessError> {
-            <dyn FieldAccess>::try_get_slice(self, field)
         }
 
         #[inline]
         pub fn set<T: Any>(&mut self, field: &str, value: T) -> Result<(), AccessError> {
             <dyn FieldAccess>::set(self, field, value)
         }
+
+        #[inline]
+        pub fn replace<T: Any>(&mut self, field: &str, value: T) -> Result<T, AccessError> {
+            <dyn FieldAccess>::replace(self, field, value)
+        }
+
+        #[inline]
+        pub fn take<T: Any + Default>(&mut self, field: &str) -> Result<T, AccessError> {
+            <dyn FieldAccess>::take(self, field)
+        }
     };
 }
 
-impl dyn FieldAccess + Send + '_ {
-    forward_field_access_methods!();
+impl dyn FieldAccess + Send {
+    forward_dyn_methods!();
 }
 
-impl dyn FieldAccess + Send + Sync + '_ {
-    forward_field_access_methods!();
+impl dyn FieldAccess + Send + Sync {
+    forward_dyn_methods!();
 }
 
 #[inline]
